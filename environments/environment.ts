60e69093f429a9343b03a190f2ca1ad85a6773e8
UMRGRS <qxrvynilp@gmail.com>
2025-10-14 12:27:16 -0600

    Integrated firebase into project

---
+++ environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCvxd2DAnXoriyDPAMxIHeGTADGct05HY8",
+    authDomain: "th-quarter-app.firebaseapp.com",
+    projectId: "th-quarter-app",
+    storageBucket: "th-quarter-app.firebasestorage.app",
+    messagingSenderId: "826873308087",
+    appId: "1:826873308087:web:f2d09837b454691dad3e32"
+  }
+};
