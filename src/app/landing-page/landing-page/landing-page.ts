4163fd1f7eaefc2272c94d9205541b6d40bbdcd3
Alejandro Borjon <alejandro.borjon@protonmail.com>
2025-10-24 11:12:36 -0600

    agergados elementos de sieño en landing page, background temporal.

--- src/app/landing-page/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page/landing-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Searchbar } from "../searchbar/searchbar";
 import { Tiendas } from "../tiendas/tiendas";
+import { Header } from "../header/header";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Searchbar, Tiendas],
+  imports: [Searchbar, Tiendas, Header],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css'
 })
