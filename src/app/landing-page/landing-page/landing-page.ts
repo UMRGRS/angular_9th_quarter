b3d289060024bc2affbcecb76cb2b87ee735f583
Pau <pau@gmail.com>
2025-11-05 18:06:14 -0800

    landing-sin-fonts

--- src/app/landing-page/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page/landing-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Searchbar } from "../searchbar/searchbar";
 import { Tiendas } from "../tiendas/tiendas";
-import { Header } from "../header/header";
+
+
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Searchbar, Tiendas, Header],
+  imports: [Searchbar, Tiendas],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css'
 })
